d129c76b6736a55c293d2a52ffa5d9e08927f306
Polina <polipet02@gmail.com>
2025-03-29 16:41:01 -0400

    created data processing for books, news and youtube datasets

---
+++ datasets/news.py
@@ -0,0 +1,81 @@
+"""
+This file contains implementation of the news articles dataset.
+"""
+from torch.utils.data import Dataset
+import os
+import pandas as pd
+
+# specify dataset file path (change to your own)
+# link to download dataset: https://www.kaggle.com/datasets/aryansingh0909/nyt-articles-21m-2000-present
+FILE_PATH = "/Users/polinapetrova/data/nyt-metadata.csv"
+
+
+class NewsDataset(Dataset):
+    """
+    Class to read data relating to NYT news articles.
+    """
+    def __init__(self, file_path=FILE_PATH, chunksize=100000):
+        """
+        This dataset is very large. 
+        It contains 2.1M instances, so it needs to be processed in chunks.
+        """
+        assert os.path.exists(file_path), f"File not found: {file_path}"
+        
+        # attributes to use (can be modified)
+        # columns i didn't include:
+        # web url
+        # news desk (dept responsible for article)
+        # byline (article authors)
+        # word count
+        # id
+        # uri
+        # print page
+        # print section
+        # source (we know all the articles came from the NYT)
+        # document type (documents type are all articles)
+        # section name
+        # multimedia
+        self.keep_columns = [
+            'abstract', 'snippet', 'lead_paragraph', 
+            'headline', 'keywords', 'pub_date', 
+            'type_of_material', 'subsection_name'
+        ]
+        # to consider:
+        # 'subsection_name' column contains many NaaN rows but indicates what topic the article relates to (ex. politics)
+        # 'type_of_material' indicates type of article (ex. news)
+        # dataset contains articles from 2000 to 2025, so we can filter which dates to keep (maybe by when the reddit posts were created)
+        # i was unsure whether to include all 3: abstract, snippet and lead_paragraph, we can discuss later
+        
+        # empty list to store chunks
+        chunks = []
+        
+        # read in chunks and append to the list
+        for chunk in pd.read_csv(file_path, usecols=self.keep_columns, chunksize=chunksize):
+            # since our focus is on news articles only, keep only those instances
+            filtered = chunk[chunk['type_of_material'] == "News"]
+            chunks.append(filtered)
+        
+        # concatenate chunks into a single dataframe
+        self.data = pd.concat(chunks, ignore_index=True)
+
+    def __getitem__(self, idx):
+        """
+        Retrieves item at specified index from the dataset.
+        :param idx: index of item
+        :return: dictionary of item's attributes
+        """
+        row = self.data.iloc[idx]
+        
+        # excluded type of material because we know they're all news
+        return {
+            "headline": row['headline'],
+            "abstract": row['abstract'], 
+            "snippet": row['snippet'], 
+            "lead_paragraph": row['lead_paragraph'], 
+            "keywords": row['keywords'], 
+            "pub_date": row['pub_date'],  
+            "topic": row['subsection_name']
+        }
+    
+    def __len__(self):
+        return len(self.data)
---
+++ datasets/youtube.py
@@ -0,0 +1,38 @@
+"""
+This file contains implementation of the YouTube comments dataset.
+"""
+from torch.utils.data import Dataset
+import os
+import pandas as pd
+
+# specify dataset file path (change to your own)
+# link to download dataset: https://www.kaggle.com/datasets/atifaliak/youtube-comments-dataset
+FILE_PATH = "/Users/polinapetrova/data/YoutubeCommentsDataSet.csv"
+
+
+class YoutubeDataset(Dataset):
+    """
+    Class to read data relating to YouTube comments.
+    """
+    def __init__(self, file_path=FILE_PATH, **kwargs):
+        assert os.path.exists(file_path), f"File not found: {file_path}"
+        
+        # read youtube comment data (csv)
+        self.data = pd.read_csv(file_path)
+
+    def __getitem__(self, idx):
+        """
+        Retrieves item at specified index from the dataset.
+        :param idx: index of item
+        :return: dictionary of item's attributes
+        """
+        # kept only column of comment text
+        # dataset also had a 'sentiment' column which i didn't include since
+        # we'll be doing our own sentiment analysis
+        comment = self.data.iloc[idx]['Comment']
+
+        return {"comment": comment.strip()}
+    
+    def __len__(self):
+        return len(self.data)
+    
