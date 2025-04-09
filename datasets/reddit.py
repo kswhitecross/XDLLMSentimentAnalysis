a83a79bb1d8f6deca5022e5cac5b0d7a16d860ca
Andrew Childs <ajchilds@gmail.com>
2025-04-09 19:23:21 -0400

    added reddit data setup code and torch Dataset class code

--- datasets/reddit.py
+++ datasets/reddit.py
@@ -2,6 +2,7 @@
 This file contanis the implementation of the reddit dataset.
 """
 from torch.utils.data import Dataset
+import pandas as pd
 
 
 class RedditDataset(Dataset):
@@ -9,7 +10,13 @@ class RedditDataset(Dataset):
     Description here.  #TODO Andrew
     """
     def __init__(self, subreddit: str):
-        self.subreddit = subreddit
+        if subreddit is None:
+            raise ValueError("subreddit name in RedditDataset constructor cannot be None")
+        file_path = "./reddit_post_comments.csv"
+        df = pd.read_csv(file_path)
+
+        #filtering by subreddit name, so this should return only 10 rows
+        self.data = df.where(df['subreddit'] == subreddit).dropna()
 
     def __getitem__(self, idx):
         """
@@ -17,7 +24,7 @@ class RedditDataset(Dataset):
         :param idx:
         :return:
         """
-        return {}
+        return {"subreddit_name": self.data.iloc[idx]["subreddit"], "post_title": self.data.iloc[idx]["post_title"], "comments": self.data.iloc[idx]["comments"]}
 
     def __len__(self) -> int:
-        return 0
+        return len(self.data)
