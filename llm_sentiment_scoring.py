b3164ab2b0bf5a08548233d8b764928ca600111b
Paige Carlson <phcarlson21@gmail.com>
2025-05-10 16:11:29 -0400

    Produce HF sentiment model distros from scratch for each in-context sample type, model type combo

--- llm_sentiment_scoring.py
+++ llm_sentiment_scoring.py
@@ -6,7 +6,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from pathlib import Path
-
+from tqdm import tqdm
+from torch.utils.data import DataLoader
 
 def load_jsonl_as_dataframe(cross_domain_run_folder_path, save_as_csv = False):
 
@@ -23,13 +24,13 @@ def load_jsonl_as_dataframe(cross_domain_run_folder_path, save_as_csv = False):
     return df
 
 
-def expected_sentiment_scores_across_subreddit_posts(df):
+def compute_expected_sentiment_scores_across_subreddit_posts(df):
     ''' Per inquiry_domain subreddit name, 
         get the expected sentiment (avg) per inq_doc_idx within that subreddit
         for both the control case and experimental case. '''  
 
     # Make sure the scores and indices numeric
-    df['score'] = df['score'].astype(int)
+    df['score'] = df['score'].astype(float)
     df['inq_doc_idx'] = df['inq_doc_idx'].astype(int)
 
     expected_score_per_post_within_domain_given_context = (
@@ -60,14 +61,15 @@ def compute_sentiment_shift_of_expectations_across_subreddit_posts(df):
 
 
 def compute_expected_sentiment_shift_per_domain(df):
-    # Ensure these are all ints 
-    df["sentiment_shift"] =  df["sentiment_shift"].astype(int)
+    # Ensure shift is a number
+    df["sentiment_shift"] = df["sentiment_shift"].astype(float)
 
-    # Now, for every in_context - inquiry domain pairing, get the expected shift from the control 
-    expected_shift_per_inquiry_given_context = df.groupby(['in_context_domain', 'inquiry_domain'])['sentiment_shift'].mean().reset_index()
-    expected_shift_per_inquiry_given_context.rename(columns={'sentiment_shift': 'expected_sentiment_shift'}, inplace=True)
+    summary = (df.groupby(['in_context_domain', 'inquiry_domain']) 
+                .agg(expected_sentiment_shift=('sentiment_shift', 'mean'),
+                     std_sentiment_shift=('sentiment_shift', 'std'))
+                .reset_index())
 
-    return expected_shift_per_inquiry_given_context
+    return summary
 
 
 def check_control_vs_control_shift(df):
@@ -83,11 +85,11 @@ def check_control_vs_control_shift(df):
         print("Control does not differ from control, as we hoped")
 
 
-def plot_heatmap(df, vmin, vmax, save_file_dir = None, save_file_name = None, show = False):
+def plot_heatmap(df, value_to_plot, vmin, vmax, save_file_dir = None, save_file_name = None, show = False):
     # Per https://seaborn.pydata.org/generated/seaborn.heatmap.html, the rows AKA index is the in-context domain
     # Cols are inquiry
     # And we use the expected sentiment shift for this
-    pivot_table = df.pivot(index='in_context_domain', columns='inquiry_domain', values='expected_sentiment_shift')
+    pivot_table = df.pivot(index='in_context_domain', columns='inquiry_domain', values=value_to_plot)
 
     plt.figure(figsize=(12, 8))
     sns.heatmap(pivot_table, annot=True, cmap="coolwarm", fmt='.4f', 
@@ -100,12 +102,32 @@ def plot_heatmap(df, vmin, vmax, save_file_dir = None, save_file_name = None, sh
     plt.tight_layout()
 
     if save_file_name != None and save_file_dir != None:
-        plt.savefig(os.path.join(save_file_dir, save_file_name))
+        save_name = os.path.join(save_file_dir, save_file_name)
+        print(f"Saving heatmap to: {save_name}")
+        plt.savefig(save_name)
 
     if show:
         plt.show()
 
-def main():
+def run_sentiment_analysis(sentiment_model, df):
+    # Get the col of answers
+    model_answers =  list(df['model_answer'])
+
+    # Num answers x 2 for pos, neg
+    distro_batch_raw = sentiment_model(model_answers)
+    cols_to_add_to_df  = [{distro['label'].lower(): distro['score'] for distro in row} for row in distro_batch_raw]
+    sentiment_df = pd.DataFrame(cols_to_add_to_df)
+
+    # In this case, there are more than one scores labels in the distro
+    # To start, lets get the shift by adding  the max label as the overall score
+    sentiment_columns = ['negative', 'positive']
+    sentiment_df['sentiment_label'] = sentiment_df[sentiment_columns].idxmax(axis=1)
+    # Then mapping that max label to a number to calc the shift
+    label_to_score = {'negative': -1, 'positive': 1}
+    sentiment_df['score'] = sentiment_df['sentiment_label'].map(label_to_score)
+    return sentiment_df
+
+def get_shifts_and_plot_from_llm_sentiments():
     # Collect this for consistent color scale
     global_shifts = []  
 
@@ -155,7 +177,7 @@ def main():
                 llama_combined_with_sentiments = llama_combined_with_sentiments[llama_combined_with_sentiments['score'].notna()]
 
                 # First get the expected sentiment per post within an inquiry domain
-                expected_sentiment_per_posts = expected_sentiment_scores_across_subreddit_posts(llama_combined_with_sentiments)
+                expected_sentiment_per_posts = compute_expected_sentiment_scores_across_subreddit_posts(llama_combined_with_sentiments)
                 # Then get the expected sentiment shift per post within an inquiry domain
                 expected_sentiment_shifts_per_posts = compute_sentiment_shift_of_expectations_across_subreddit_posts(expected_sentiment_per_posts)
                 # Lastly get the average of these expected shifts per post, for the expected shift per inquiry domain given in-context domain
@@ -178,6 +200,7 @@ def main():
             
             save_file_name = model_name + "_" + num_in_context_type + '_heatmap.png'
             plot_heatmap(df=shift_df,
+                        value_to_plot='expected_sentiment_shift',
                         save_file_dir=num_in_context_type,
                         save_file_name=save_file_name,
                         show=False,
@@ -186,11 +209,161 @@ def main():
             
             save_file_name = model_name + "_" + num_in_context_type + '_standardized_heatmap.png'
             plot_heatmap(df=shift_df,
+                        value_to_plot='expected_sentiment_shift',
+                        save_file_dir=num_in_context_type,
+                        save_file_name=save_file_name,
+                        show=False,
+                        vmin=global_shift_min,
+                        vmax=global_shift_max)
+            
+
+            save_file_name = model_name + "_" + num_in_context_type + '_std_heatmap.png'
+            plot_heatmap(df=shift_df,
+                        value_to_plot='std_sentiment_shift',
+                        save_file_dir=num_in_context_type,
+                        save_file_name=save_file_name,
+                        show=False,
+                        vmin=None,
+                        vmax=None)
+            
+            save_file_name = model_name + "_" + num_in_context_type + '_standardized_std_heatmap.png'
+            plot_heatmap(df=shift_df,
+                        value_to_plot='std_sentiment_shift',
+                        save_file_dir=num_in_context_type,
+                        save_file_name=save_file_name,
+                        show=False,
+                        vmin=global_shift_min,
+                        vmax=global_shift_max)
+            
+
+def get_shifts_and_plot_from_hf_sentiment_distros(sentiment_model):
+    # Collect this for consistent color scale
+    global_shifts = []  
+
+    # So ya don't need to recalculate everything
+    expected_shifts_cache = [] 
+
+    for num_in_context_type in ['long', 'original']:
+          
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print(f"ANALYZING LLAMA MODELS with {num_in_context_type} IN-CONTEXT SAMPLES....")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+        folder = Path(num_in_context_type)
+        for sub_folder in folder.iterdir():
+            if sub_folder.is_dir():
+                      
+
+                # Get the model name of the current directory of results
+                sub_folder_name_parts = sub_folder.name.split("_")
+                # if sub_folder_name_parts[1] == '70B':
+                #     model_name = "_".join(sub_folder_name_parts[:3])
+                # else:
+                #     model_name = "_".join(sub_folder_name_parts[:2])
+                model_name = "_".join(sub_folder_name_parts[:2])
+
+
+        
+                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                print(f"GETTING HF SCORE DISTROS WITH {num_in_context_type}, {model_name}....")
+                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+                llama_outputs =  load_jsonl_as_dataframe(os.path.join(sub_folder, 'results.jsonl'), save_as_csv = False)
+                # respective_sentiment_scores = run_sentiment_analysis(sentiment_model, llama_outputs)
+                # # Sentiments for this num context type, this model version
+                # output_dir = os.path.join("hf_sentiment_from_scratch", num_in_context_type)
+                # # If dir already exists that is fine otherwise make it
+                # os.makedirs(output_dir, exist_ok=True)
+                # # We want the same structure as with sentiment folder from the LLM scores
+                # sentiment_save_path = os.path.join(output_dir, f"{model_name}.jsonl")
+                # respective_sentiment_scores.to_json(sentiment_save_path, orient='records', lines=True)
+
+                respective_sentiment_scores = load_jsonl_as_dataframe(os.path.join('hf_sentiment_from_scratch', num_in_context_type, model_name + '.jsonl'), save_as_csv = False)
+
+                # Add the sentiment scores as cols
+                llama_combined_with_sentiments =  pd.concat([llama_outputs, respective_sentiment_scores['score']], axis=1)
+                
+                # Just a sanity check that our scores in fact do align fully with the OG output rows
+                print("Llama outputs shape:", llama_outputs.shape)
+                print("Sentiment scores shape:", respective_sentiment_scores.shape)
+                print("Merged shape:", llama_combined_with_sentiments.shape)
+                print("# Null doc idx:", llama_combined_with_sentiments['inq_doc_idx'].isna().sum())
+                print("# Null scores:", llama_combined_with_sentiments['score'].isna().sum())
+                print("The NaN rows, if any:", respective_sentiment_scores[respective_sentiment_scores['score'].isna()])
+
+                # TODO check in on this
+                llama_combined_with_sentiments = llama_combined_with_sentiments[llama_combined_with_sentiments['score'].notna()]
+
+
+        
+                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                print(f"CALCULATING SHIFTS WITH {num_in_context_type}, {model_name}....")
+                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+                # First get the expected sentiment per post within an inquiry domain
+                expected_sentiment_per_posts = compute_expected_sentiment_scores_across_subreddit_posts(llama_combined_with_sentiments)
+                # Then get the expected sentiment shift per post within an inquiry domain
+                expected_sentiment_shifts_per_posts = compute_sentiment_shift_of_expectations_across_subreddit_posts(expected_sentiment_per_posts)
+                # Lastly get the average of these expected shifts per post, for the expected shift per inquiry domain given in-context domain
+                expected_sentiment_shifts_per_inquiry_domain = compute_expected_sentiment_shift_per_domain(expected_sentiment_shifts_per_posts)
+                # Sanity check that the shift calcs are 0 for control vs control
+                # check_control_vs_control_shift(expected_sentiment_shifts_per_inquiry_domain)
+
+                # Collect shift values for global vmin/vmax
+                global_shifts.extend(expected_sentiment_shifts_per_inquiry_domain['expected_sentiment_shift'].tolist())
+                # Cache for second pass
+                expected_shifts_cache.append((num_in_context_type, model_name, expected_sentiment_shifts_per_inquiry_domain))
+    
+    global_shift_max = max(global_shifts)
+    global_shift_min = min(global_shifts)
+
+    for num_in_context_type, model_name, shift_df in expected_shifts_cache:
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            print(f"PLOTTING HEATMAP FOR {num_in_context_type}, {model_name}....")
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            
+            save_file_name = model_name + "_" + num_in_context_type + '_hf_sentiment_binary_heatmap.png'
+            plot_heatmap(df=shift_df,
+                        value_to_plot='expected_sentiment_shift',
+                        save_file_dir=num_in_context_type,
+                        save_file_name=save_file_name,
+                        show=False,
+                        vmin=None,
+                        vmax=None)
+            
+            save_file_name = model_name + "_" + num_in_context_type + '_hf_sentiment_binary_standardized_heatmap.png'
+            plot_heatmap(df=shift_df,
+                        value_to_plot='expected_sentiment_shift',
+                        save_file_dir=num_in_context_type,
+                        save_file_name=save_file_name,
+                        show=False,
+                        vmin=global_shift_min,
+                        vmax=global_shift_max)
+            
+
+            save_file_name = model_name + "_" + num_in_context_type + '_hf_sentiment_binary_std_heatmap.png'
+            plot_heatmap(df=shift_df,
+                        value_to_plot='std_sentiment_shift',
+                        save_file_dir=num_in_context_type,
+                        save_file_name=save_file_name,
+                        show=False,
+                        vmin=None,
+                        vmax=None)
+            
+            save_file_name = model_name + "_" + num_in_context_type + '_hf_sentiment_binary__standardized_std_heatmap.png'
+            plot_heatmap(df=shift_df,
+                        value_to_plot='std_sentiment_shift',
                         save_file_dir=num_in_context_type,
                         save_file_name=save_file_name,
                         show=False,
                         vmin=global_shift_min,
                         vmax=global_shift_max)
         
+
+def main():
+    #  distilbert/distilbert-base-uncased-finetuned-sst-2-english
+    sentiment_model = pipeline("sentiment-analysis", truncation=True, top_k=None)
+    get_shifts_and_plot_from_hf_sentiment_distros(sentiment_model)
+
 if __name__ == "__main__":
     main()
